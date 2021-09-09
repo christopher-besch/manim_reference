139aafd2750e43ee5b413f5175616b915f505624
Christopher Besch <christopher.besch@gmx.de>
2021-09-09 13:16:30 +0200

    added complex animation

--- gallery/scene.py
+++ gallery/scene.py
@@ -50,11 +50,64 @@ class PointMovingOnShapes(Scene):
         self.add(line)
 
         self.play(GrowFromCenter(circle))
-        self.play(Transform(dot, dot2))
-        # too short timing with this
-        # dot.animate.shift(RIGHT)
+        # both work
+        # self.play(Transform(dot, dot2))
+        self.play(dot.animate.shift(RIGHT))
         # move dot along circle
         self.play(MoveAlongPath(dot, circle), run_time=2, rate_func=linear)
         # rotate dot in mid-air
         self.play(Rotating(dot, about_point=[2, 0, 0]), run_time=1.5)
         self.wait()
+
+
+class MovingAround(Scene):
+    def construct(self):
+        square = Square(color=BLUE, fill_opacity=1)
+
+        self.play(square.animate.shift(LEFT))
+        self.play(square.animate.set_fill(ORANGE))
+        self.play(square.animate.scale(0.3))
+        self.play(square.animate.rotate(0.4))
+
+
+class MovingAngle(Scene):
+    def construct(self):
+        rotation_center = LEFT
+        theta_tracker = ValueTracker(110)
+        # unmoving horizontal line
+        line = Line(LEFT, RIGHT)
+        line_ref = line.copy()
+
+        line_moving = Line(LEFT, RIGHT)
+        # rotate to initial setting
+        line_moving.rotate(theta_tracker.get_value() * DEGREES, about_point=rotation_center)
+        a = Angle(line, line_moving, radius=0.5, other_angle=False)
+        tex = MathTex(r"\theta").move_to(
+            # get middle of arch of virtual angle
+            Angle(line, line_moving, radius=0.5 + 3 * SMALL_BUFF, other_angle=False).point_from_proportion(0.5)
+        )
+
+        self.add(line, line_moving, a, tex)
+        self.wait()
+
+        line_moving.add_updater(
+            lambda x: x.become(line_ref.copy()).rotate(
+                theta_tracker.get_value() * DEGREES, about_point=rotation_center
+            )
+        )
+
+        a.add_updater(
+            lambda x: x.become(Angle(line, line_moving, radius=0.5, other_angle=False))
+        )
+        tex.add_updater(
+            lambda x: x.move_to(
+                Angle(
+                    line, line_moving, radius=0.5 + 3 * SMALL_BUFF, other_angle=False
+                ).point_from_proportion(0.5)
+            )
+        )
+
+        self.play(theta_tracker.animate.set_value(40))
+        self.play(theta_tracker.animate.increment_value(140))
+        self.play(tex.animate.set_color(RED), run_time=0.5)
+        self.play(theta_tracker.animate.set_value(350))
