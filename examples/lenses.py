816d209d2a48e19067bb442ac0a8c4739cc45a51
christopher-besch <christopher.besch@gmx.de>
2021-11-17 20:54:21 +0100

    can we have lense at home?

--- examples/lenses.py
+++ examples/lenses.py
@@ -3,26 +3,18 @@ from manim import *
 
 class LenseExample(Scene):
     def construct(self):
-        c1 = Circle(2).move_to(1*LEFT)
-        c2 = Circle(2).move_to(1*RIGHT)
-        lense = Intersection(c1, c2, color=YELLOW, fill_opacity=1, stroke_width=0)
-        self.add(lense, c2)
 
-        # self.shoot_ray(c1, c2, 1, 1.0, 1.4)
-        # self.shoot_ray(c1, c2, 0.5, 1.0, 1.4)
-        # self.shoot_ray(c1, c2, 0, 1.0, 1.4)
-        # self.shoot_ray(c1, c2, -0.5, 1.0, 1.4)
-        # self.shoot_ray(c1, c2, -1, 1.0, 1.4)
+        c1 = Circle(20).move_to(19.5*LEFT)
+        c2 = Circle(20).move_to(19.5*RIGHT)
+        lense = Intersection(c1, c2, color=BLUE, fill_opacity=1, stroke_width=0)
+        self.add(lense)
 
-        start = Point((-2, 0, 0))
-        end = Point(self.intersect(c2.get_center(), c2.radius, start.get_center(), 2*RIGHT + UP))
-        line = Line(start, end)
-        self.add(start, end, line)
+        for i in np.arange(-4.5, 4.5, 0.01):
+            self.shoot_ray(c1, c2, i, 1.0, 1.4, ORANGE)
 
     def intersect(self, circle_center, radius, ray_origin, inclination):
         """Intersection between circle and ray."""
         center_to_origin = ray_origin - circle_center
-        # inclination = normalize(inclination)
 
         a = inclination.dot(inclination)
         half_b = center_to_origin.dot(inclination)
@@ -32,28 +24,38 @@ class LenseExample(Scene):
         assert discriminant >= 0
 
         # find nearest root
-        t = (-half_b - np.sqrt(discriminant) / a)
-        # if (t < 0):
-        # t = (-b + np.sqrt(discriminant) / 2*a)
+        t = (-half_b - np.sqrt(discriminant)) / a
+        if (t < 0):
+            t = (-half_b + np.sqrt(discriminant)) / a
         return ray_origin + t * inclination
 
-    def shoot_ray(self, c1: Circle, c2: Circle, start_y, eta_air, eta_glass):
-        start = Point((-4, start_y, 0))
-        inter1 = Point(self.intersect(c2.get_center(), c2.radius, start.get_center(), UR, True))
-        self.add(inter1)
-        ray1 = Line(start, inter1)
+    def refract(self, c: Circle, ray_origin, inclination, eta_air, eta_glass, into):
+        inter = Point(self.intersect(c.get_center(), c.radius, ray_origin, inclination))
+        ray = Line(ray_origin, inter)
+
+        norm = inter.get_center() - c.get_center()
+        theta = angle_between_vectors(ray.get_vector(), -norm)
+        # get signed angle
+        if ray.get_vector().dot(rotate_vector(norm, PI/2)) < 0:
+            theta = -theta
+        if into:
+            theta_p = np.arcsin(eta_air/eta_glass*np.sin(theta))
+            out_vec = rotate_vector(-norm, theta_p)
+        else:
+            theta_p = np.arcsin(eta_glass/eta_air*np.sin(theta))
+            out_vec = rotate_vector(norm, theta_p)
+
+        return inter, out_vec
+
+    def shoot_ray(self, c1: Circle, c2: Circle, start_y, eta_air, eta_glass, color):
+        start = Point((-8, start_y, 0))
+        inter1, vec1 = self.refract(c2, start.get_center(), RIGHT, eta_air, eta_glass, True)
+        ray1 = Line(start, inter1, color=color, stroke_width=1)
         self.add(ray1)
 
-        norm1_vec = inter1.get_center() - c2.get_center()
-        norm1 = Line(c2.get_center(), inter1)
-        theta = angle_between_vectors(ray1.get_vector(), -norm1_vec)
-        # get signed angle
-        if ray1.get_vector().dot(rotate_vector(norm1_vec, PI/2)) < 0:
-            theta = -theta
-
-        r = Line(inter1, inter1.get_center() + rotate_vector(-norm1_vec, theta))
-        # self.add(r)
-
-        inter2 = Point(self.intersect(c1.get_center(), c1.radius, inter1.get_center(), DL, False))
-        ray2 = Line(inter1, inter2)
+        inter2, vec2 = self.refract(c1, start.get_center(), vec1, eta_air, eta_glass, False)
+        ray2 = Line(inter1, inter2, color=color, stroke_width=1)
         self.add(ray2)
+
+        ray3 = Line(inter2, inter2.get_center() + 3*vec2, color=color, stroke_width=1)
+        self.add(ray3)
